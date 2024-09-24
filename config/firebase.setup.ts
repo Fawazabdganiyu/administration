54d4dde5ee8c01b1a17d2e4f984b288251cf40e0
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-09-24 10:12:58 +0100

    Remove hardcoded path

--- config/firebase.setup.ts
+++ config/firebase.setup.ts
@@ -9,7 +9,10 @@ export class FirebaseAdmin implements OnApplicationBootstrap {
   async onApplicationBootstrap() {
     if (!app) {
       const serviceAccount = JSON.parse(
-        await readFile('./config/firebaseServiceAccountKey.json', 'utf-8'),
+        await readFile(
+          process.env.FIREBASE_SERVICE_ACCOUNT_PATH as string,
+          'utf-8',
+        ),
       );
       app = admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
